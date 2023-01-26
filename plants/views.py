2b353c0c8ead14c06c1f6f6323fcb30670b56662
Skevin98 <sawadogokevin98@gmail.com>
2023-01-26 18:48:00 +0100

    branch merging

--- plants/views.py
+++ plants/views.py
@@ -9,50 +9,58 @@ from plants.datatest import dataset, firstThreeFlowers
 api_key = "BHZ6vXLWZhSu681vE647FyCh8hb2VieY8sTervUb91LT-wJymtVQUqkqyiFp5owI"
 
 
+plants = []
+
+
 def index(request):
     """Vue affichant la page d'accueil"""
-    # print("+++++ : ",request.GET.get('queue', 'pas de queue'))
-    # if 'queue' not in request.GET or request.GET['queue'] == '':
-    #     queue = 'red'
-    # else:
-    #     queue = request.GET['queue']
+    print("+++++ : ",request.GET.get('queue', 'pas de queue'))
+    if 'queue' not in request.GET or request.GET['queue'] == '':
+        queue = 'red'
+    else:
+        queue = request.GET['queue']
          
-    # try:
-    #     response = requests.get("https://api.floracodex.com/v1/plants?key=%s&q=%s"%(api_key, queue))
+    try:
+        response = requests.get("https://api.floracodex.com/v1/plants?key=%s&q=%s"%(api_key, queue))
         
-    # except:
-    #     pass
-    # else:
-    #     responseJSON = response.json()
-    #     pprint(responseJSON)
-    #     plants = responseJSON['data'][:10]
-    #     print("######## queue : ", queue)
-    #     #pprint(plants)
-    #     return render(request, 'base.html',{'plants':plants})
-    plants = dataset['data']
-    return render(request, 'base.html',{'plants': enumerate(plants)})
+    except:
+        pass
+    else:
+        global plants
+        responseJSON = response.json()
+        #pprint(responseJSON['data'][:10])
+        plants = list(responseJSON['data'][:10]).copy()
+        pprint(plants)
+
+        print("######## queue : ", queue)
+        #pprint(plants)
+        return render(request, 'base.html',{'plants': enumerate(plants)})
+    # plants = dataset['data']
+    # return render(request, 'base.html',{'plants': enumerate(plants)})
     
     
 def ajax_index(request):
     """requete ajax permettant de faire une nouvelle recherche sans actualiser la page"""
-    # print("+++++ in ajax : ",request.GET.get('queue', 'pas de queue'))
-    # if 'queue' not in request.GET or request.GET['queue'] == '':
-    #     queue = 'red'
-    # else:
-    #     queue = request.GET['queue']
+    print("+++++ in ajax : ",request.GET.get('queue', 'pas de queue'))
+    if 'queue' not in request.GET or request.GET['queue'] == '':
+        queue = 'red'
+    else:
+        queue = request.GET['queue']
          
-    # try:
-    #     response = requests.get("https://api.floracodex.com/v1/plants?key=%s&q=%s"%(api_key, queue))
+    try:
+        response = requests.get("https://api.floracodex.com/v1/plants?key=%s&q=%s"%(api_key, queue))
         
-    # except:
-    #     pass
-    # else:
-    #     responseJSON = response.json()
-    #     pprint(responseJSON)
-    #     plants = responseJSON['data'][:10]
+    except:
+        pass
+    else:
+        global plants
+        responseJSON = response.json()
+        #pprint(responseJSON['data'][:10])
+        plants = list(responseJSON['data'][:10]).copy()
+        pprint(plants)
 
-    #     return render(request,'query.html',{'plants':plants})
-    pass
+        return render(request,'query.html',{'plants': enumerate(plants)})
+
 
 
 
@@ -60,7 +68,7 @@ def detail(request, id):
     """Retourne la vue affichant les details d'une fleur selectionnée dans la liste"""
     if id != None:
         #flower contient les infos de la fleur numero id. 
-        flower = firstThreeFlowers[id]
+        # flower = firstThreeFlowers[id]
         
         
         #++++++++++++++        Travail à Faire         +++++++++++++++++++++++++++++++++
@@ -70,6 +78,9 @@ def detail(request, id):
         # Les données des 3 premieres fleurs sont disponibles dans datatest.py
         
         #voir la fonction index comme exemple 
+        print("liste longueur : ", len(plants))
+        flower = plants[id]
+        
         return render(request,'detail.html',{'flower':flower}) #Effacer ce return et ecrire le votre
         
 # Create your views here.
